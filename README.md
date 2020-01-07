d7a467e7202c0e811f622289e45916bf39acd4b1
simaQ <sima.zhang1990@gmail.com>
2020-01-07 16:12:51 +0800

    sync

--- README.md
+++
@@ -1,3 +0,0 @@
-# g2-3.x-site
-
-G2 3.x 网站。新版站点请访问： https://antv.alipay.com/
--- node_modules/_@antv_gatsby-theme-antv@0.10.32@@antv/gatsby-theme-antv/lib/components/Search.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export interface SearchProps {
-    docsearchOptions?: {
-        apiKey: string;
-        indexName: string;
-    };
-}
-declare const Search: React.FC<SearchProps>;
-export default Search;
